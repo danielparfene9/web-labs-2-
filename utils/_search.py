490320b6c406c2d7011189e39c656e4e4dfd9c87
danielparfene9 <daniel.parfene@isa.utm.md>
2025-03-18 01:16:09 +0200

    encapsulate search add excp

--- utils/_search.py
+++ utils/_search.py
@@ -1,13 +1,32 @@
-from ._utils import Client, ENGINE_URL, re, List, Optional
+from ._utils import Client, ENGINE_URL, re, List
 
-def search_term(term):
-    search_url = ENGINE_URL + "+".join(term.split())
-    response = Client.send_request(search_url)
-    readable_text = Client._parse_http_response(response)
+class SearchError(Exception):
+    pass
 
-    results = re.findall(r'<a rel="nofollow" class="result__a" href="(.*?)">(.*?)</a>', readable_text)
-    if not results:
-        return "No search results found."
+class SearchEngine:
 
-    output = "\n".join([f"{i+1}. {title} - {link}" for i, (link, title) in enumerate(results[:10])])
-    return output
+    @staticmethod
+    def search_term(term: str) -> str:
+        search_url = f"{ENGINE_URL}{'+'.join(term.split())}"
+
+        response = Client.send_request(search_url)
+        if not response:
+            raise SearchError("Failed to fetch search results.")
+
+        readable_text = Client._parse_http_response(response)
+        if not readable_text:
+            raise SearchError("Could not extract text from the response.")
+
+        results = SearchEngine._extract_links(readable_text)
+        if not results:
+            raise SearchError("No search results found.")
+
+        return SearchEngine._format_results(results)
+
+    @staticmethod
+    def _extract_links(html_content: str) -> List[tuple]:
+        return re.findall(r'<a rel="nofollow" class="result__a" href="(.*?)">(.*?)</a>', html_content)
+
+    @staticmethod
+    def _format_results(results: List[tuple]) -> str:
+        return "\n".join([f"{i+1}. {title} - {link}" for i, (link, title) in enumerate(results[:10])])
