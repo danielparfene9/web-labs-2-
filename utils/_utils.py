cf219758bc21736897e6b3ab79ff164a6fde9bec
danielparfene9 <daniel.parfene@isa.utm.md>
2025-03-18 01:17:09 +0200

    export class

--- utils/_utils.py
+++ utils/_utils.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, Field
 
 from ._http_request import HTTPClient as Client
 from ._parser import HTMLExtractor as Parser
+from ._search import SearchEngine as Search
 
 USER_AGENT = "go2web-cli/1.0"
 CACHE_DIR = ".go2web_cache"
@@ -21,5 +22,6 @@ __all__ = [
     "BaseModel",
     "Field",
     "Client",
-    "Parser"
+    "Parser",
+    "Search"
 ]
