9cbb818332359899bfcac769d4eb9701e458736c
danielparfene9 <daniel.parfene@isa.utm.md>
2025-03-18 01:54:42 +0200

    remove custom -h

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ def main():
     parser = argparse.ArgumentParser(description="go2web: A CLI tool for making HTTP requests and searching the web.")
     parser.add_argument("-u", "--url", type=str, help="Fetch content from a URL")
     parser.add_argument("-s", "--search", type=str, help="Search the web for a term")
-    parser.add_argument("-h", "--help", action="store_true", help="Show help")
-    
+
     args = parser.parse_args()
     
-    if args.help:
-        parser.print_help()
-    elif args.url:
+    if args.url:
         print(Client.send_request(args.url))
     elif args.search:
         print(Search.search_term(args.search))
